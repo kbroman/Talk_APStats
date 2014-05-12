144627026c56cbf63a94630fb4fe76c697d63049
Karl Broman <kbroman@gmail.com>
2014-05-12 18:41:09 -0500

    Revise plots of growth curves and growth rate

--- R/growth_curves.R
+++ R/growth_curves.R
@@ -9,94 +9,124 @@ ymax <- max(c(unlist(phe[,wkcol]), unlist(f2$pheno[,1:16])), na.rm=TRUE)
 
 library(broman)
 bg <- brocolors("bg")
-
-pdf("../Figs/growth1.pdf", height=6.5, width=10, pointsize=20)
-par(bg=bg, fg="white", col="white", col.axis="white", col.lab="white")
-par(las=1, mar=c(4.1, 4.1, 0.6, 3.1))
-xat <- c(5, 10, 15)
-yat <- seq(0, 40, by=5)
-grayplot(0,0,type="n", xlim=c(1, 16), xaxs="i", ylim=c(0, ymax*1.02), yaxs="i",
-         xlab="Week", ylab="Body weight (g)",
-         xat=c(1,5,10,15), vlines=xat, vlines.col="gray70",vlines.lwd=3,
-         yat=yat, hlines=yat)
-col <- brocolors("crayons")[c("Tickle Me Pink", "Blue Gray", "Razzmatazz", "Midnight Blue", "Wild Strawberry", "Green Blue")]
+col <- brocolors("crayons")[c("Tickle Me Pink", "Blue Gray",
+                              "Razzmatazz", "Midnight Blue",
+                              "Wild Strawberry", "Green Blue",
+                              "Razzle Dazzle Rose", "Blue Violet")]
 colalpha <- colwalpha(col, alpha=0.3)
-for(i in sample(nrow(parents))) {
-  y <- parents[i,wkcol]
-  x <- (1:16)[!is.na(y)]
-  y <- y[!is.na(y)]
-  lines(x, y, lwd=2, col=col[par_sex[i] + 2*(par_strain[i]-1)])
-}
-text(11.5, 12.5, "WSB", adj=c(0, 0.5), col="black")
-text(6, 34, "Gough", adj=c(0, 1), col="gray40")
-dev.off()    
 
+gmeanM <- colMeans(parents[par_strain==1 & par_sex==2,wkcol], na.rm=TRUE)
+wmeanM <- colMeans(parents[par_strain==2 & par_sex==2,wkcol], na.rm=TRUE)
+gmeanF <- colMeans(parents[par_strain==1 & par_sex==1,wkcol], na.rm=TRUE)
+wmeanF <- colMeans(parents[par_strain==2 & par_sex==1,wkcol], na.rm=TRUE)
+f2meanM <- colMeans(f2$pheno[f2$pheno$sex=="M",1:16], na.rm=TRUE)
+f2meanF <- colMeans(f2$pheno[f2$pheno$sex=="F",1:16], na.rm=TRUE)
 
-pdf("../Figs/growth2.pdf", height=6.5, width=10, pointsize=20)
-par(bg=bg, fg="white", col="white", col.axis="white", col.lab="white")
-par(las=1, mar=c(4.1, 4.1, 0.6, 3.1))
+pdf("../Figs/growth1.pdf", height=5.5, width=10, pointsize=16)
+par(bg=bg, fg="white", col="white", col.axis="white", col.lab="white", col.main="white")
+par(las=1, mar=c(4.1, 4.1, 2.6, 0.6),
+    mfrow=c(1,2))
 xat <- c(5, 10, 15)
 yat <- seq(0, 40, by=5)
 grayplot(0,0,type="n", xlim=c(1, 16), xaxs="i", ylim=c(0, ymax*1.02), yaxs="i",
          xlab="Week", ylab="Body weight (g)",
          xat=c(1,5,10,15), vlines=xat, vlines.col="gray70",vlines.lwd=3,
-         yat=yat, hlines=yat)
+         yat=yat, hlines=yat, main="Males")
 for(i in sample(which(f2$pheno$sex=="M"))) {
   y <- f2$pheno[i,1:16]
   x <- (1:16)[!is.na(y)]
   y <- y[!is.na(y)]
   lines(x, y, lwd=1, col=colalpha[2])
 }
-dev.off()    
+lines(1:16, f2meanM, col=col[4], lwd=2)
 
-
-pdf("../Figs/growth3.pdf", height=6.5, width=10, pointsize=20)
-par(bg=bg, fg="white", col="white", col.axis="white", col.lab="white")
-par(las=1, mar=c(4.1, 4.1, 0.6, 3.1))
-xat <- c(5, 10, 15)
-yat <- seq(0, 40, by=5)
 grayplot(0,0,type="n", xlim=c(1, 16), xaxs="i", ylim=c(0, ymax*1.02), yaxs="i",
          xlab="Week", ylab="Body weight (g)",
          xat=c(1,5,10,15), vlines=xat, vlines.col="gray70",vlines.lwd=3,
-         yat=yat, hlines=yat)
-for(i in sample(which(f2$pheno$sex=="M"))) {
-  y <- f2$pheno[i,1:16]
-  x <- (1:16)[!is.na(y)]
-  y <- y[!is.na(y)]
-  lines(x, y, lwd=1, col=colalpha[2])
-}
-gmean <- colMeans(parents[par_strain==1 & par_sex==2,wkcol], na.rm=TRUE)
-wmean <- colMeans(parents[par_strain==2 & par_sex==2,wkcol], na.rm=TRUE)
-lines(1:16, gmean, col=col[6], lwd=2)
-lines(1:16, wmean, col=col[4], lwd=2)
-f2mean <- colMeans(f2$pheno[f2$pheno$sex=="M",1:16], na.rm=TRUE)
-lines(1:16, f2mean, col=brocolors("crayons")["Navy Blue"], lwd=2)
-axis(side=4, at=c(gmean[16], f2mean[16], wmean[16]), label=c("Gough", expression(F[2]), "WSB"),
-     tick=FALSE, mgp=c(0, 0.3, 0))
-dev.off()    
-
-
-pdf("../Figs/growth4.pdf", height=6.5, width=10, pointsize=20)
-par(bg=bg, fg="white", col="white", col.axis="white", col.lab="white")
-par(las=1, mar=c(4.1, 4.1, 0.6, 3.1))
-xat <- c(5, 10, 15)
-yat <- seq(0, 40, by=5)
-grayplot(0,0,type="n", xlim=c(1, 16), xaxs="i", ylim=c(0, ymax*1.02), yaxs="i",
-         xlab="Week", ylab="Body weight (g)",
-         xat=c(1,5,10,15), vlines=xat, vlines.col="gray70",vlines.lwd=3,
-         yat=yat, hlines=yat)
+         yat=yat, hlines=yat, main="Females")
 for(i in sample(which(f2$pheno$sex=="F"))) {
   y <- f2$pheno[i,1:16]
   x <- (1:16)[!is.na(y)]
   y <- y[!is.na(y)]
   lines(x, y, lwd=1, col=colalpha[1])
 }
-gmean <- colMeans(parents[par_strain==1 & par_sex==1,wkcol], na.rm=TRUE)
-wmean <- colMeans(parents[par_strain==2 & par_sex==1,wkcol], na.rm=TRUE)
-lines(1:16, gmean, col=col[5], lwd=2)
-lines(1:16, wmean, col=col[3], lwd=2)
-f2mean <- colMeans(f2$pheno[f2$pheno$sex=="F",1:16], na.rm=TRUE)
-lines(1:16, f2mean, col=brocolors("crayons")["Radical Red"], lwd=2)
-axis(side=4, at=c(gmean[16], f2mean[16], wmean[16]-1), label=c("Gough", expression(F[2]), "WSB"),
-     tick=FALSE, mgp=c(0, 0.3, 0))
-dev.off()    
+lines(1:16, f2meanF, col=col[3], lwd=2)
+dev.off()
+
+
+
+
+pdf("../Figs/growth2.pdf", height=5.5, width=10, pointsize=16)
+par(bg=bg, fg="white", col="white", col.axis="white", col.lab="white", col.main="white")
+par(las=1, mar=c(4.1, 4.1, 2.6, 0.6),
+    mfrow=c(1,2))
+xat <- c(5, 10, 15)
+yat <- seq(0, 40, by=5)
+grayplot(0,0,type="n", xlim=c(1, 16), xaxs="i", ylim=c(0, ymax*1.02), yaxs="i",
+         xlab="Week", ylab="Body weight (g)",
+         xat=c(1,5,10,15), vlines=xat, vlines.col="gray70",vlines.lwd=3,
+         yat=yat, hlines=yat, main="Males")
+for(i in sample(which(f2$pheno$sex=="M"))) {
+  y <- f2$pheno[i,1:16]
+  x <- (1:16)[!is.na(y)]
+  y <- y[!is.na(y)]
+  lines(x, y, lwd=1, col=colalpha[2])
+}
+lines(1:16, f2meanF, col=col[3], lwd=2)
+lines(1:16, f2meanM, col=col[4], lwd=2)
+
+grayplot(0,0,type="n", xlim=c(1, 16), xaxs="i", ylim=c(0, ymax*1.02), yaxs="i",
+         xlab="Week", ylab="Body weight (g)",
+         xat=c(1,5,10,15), vlines=xat, vlines.col="gray70",vlines.lwd=3,
+         yat=yat, hlines=yat, main="Females")
+for(i in sample(which(f2$pheno$sex=="F"))) {
+  y <- f2$pheno[i,1:16]
+  x <- (1:16)[!is.na(y)]
+  y <- y[!is.na(y)]
+  lines(x, y, lwd=1, col=colalpha[1])
+}
+lines(1:16, f2meanM, col=col[4], lwd=2)
+lines(1:16, f2meanF, col=col[3], lwd=2)
+dev.off()
+
+
+pdf("../Figs/growth3.pdf", height=5.5, width=10, pointsize=16)
+par(bg=bg, fg="white", col="white", col.axis="white", col.lab="white", col.main="white")
+par(las=1, mar=c(4.1, 4.1, 2.6, 0.6),
+    mfrow=c(1,2))
+xat <- c(5, 10, 15)
+yat <- seq(0, 40, by=5)
+grayplot(0,0,type="n", xlim=c(1, 16), xaxs="i", ylim=c(0, ymax*1.02), yaxs="i",
+         xlab="Week", ylab="Body weight (g)",
+         xat=c(1,5,10,15), vlines=xat, vlines.col="gray70",vlines.lwd=3,
+         yat=yat, hlines=yat, main="Males")
+for(i in sample(which(f2$pheno$sex=="M"))) {
+  y <- f2$pheno[i,1:16]
+  x <- (1:16)[!is.na(y)]
+  y <- y[!is.na(y)]
+  lines(x, y, lwd=1, col=colalpha[2])
+}
+lines(1:16, f2meanM, col=col[4], lwd=2)
+lines(1:16, gmeanM, col=col[6], lwd=2)
+lines(1:16, wmeanM, col=col[8], lwd=2)
+text(9, 22.5, expression(F[2]), col=col[4], adj=c(0, 0.5))
+text(8, 27.5, "Gough", col=col[6], adj=c(1, 0.5))
+text(11, 13.5, "WSB", col=col[8], adj=c(0, 0.5))
+
+grayplot(0,0,type="n", xlim=c(1, 16), xaxs="i", ylim=c(0, ymax*1.02), yaxs="i",
+         xlab="Week", ylab="Body weight (g)",
+         xat=c(1,5,10,15), vlines=xat, vlines.col="gray70",vlines.lwd=3,
+         yat=yat, hlines=yat, main="Females")
+for(i in sample(which(f2$pheno$sex=="F"))) {
+  y <- f2$pheno[i,1:16]
+  x <- (1:16)[!is.na(y)]
+  y <- y[!is.na(y)]
+  lines(x, y, lwd=1, col=colalpha[1])
+}
+lines(1:16, f2meanF, col=col[3], lwd=2)
+lines(1:16, gmeanF, col=col[5], lwd=2)
+lines(1:16, wmeanF, col=col[7], lwd=2)
+text(9, 19, expression(F[2]), col=col[3], adj=c(0, 0.5))
+text(7, 22.5, "Gough", col=col[5], adj=c(1, 0.5))
+text(11, 12.5, "WSB", col=col[7], adj=c(0, 0.5))
+dev.off()
--- R/growth_rate.R
+++ R/growth_rate.R
@@ -26,98 +26,117 @@ par_growthSm[is.na(par_growthSm2)] <- par_growthDeriv[is.na(par_growthSm2)] <- N
 colnames(par_growthSm) <- colnames(par_growthDeriv) <- as.character(1:16)
 
 
-col <- brocolors("crayons")[c("Tickle Me Pink", "Blue Gray", "Razzmatazz", "Midnight Blue", "Wild Strawberry", "Green Blue")]
+col <- brocolors("crayons")[c("Tickle Me Pink", "Blue Gray",
+                              "Razzmatazz", "Midnight Blue",
+                              "Wild Strawberry", "Green Blue",
+                              "Razzle Dazzle Rose", "Blue Violet")]
 colalpha <- colwalpha(col, alpha=0.3)
 
-pdf("../Figs/rate1.pdf", height=6.5, width=10, pointsize=20)
-par(bg=bg, fg="white", col="white", col.axis="white", col.lab="white")
-par(las=1, mar=c(4.1, 4.1, 0.6, 3.1))
+
+gmeanM <- colMeans(par_growthDeriv[par_strain==1 & par_sex==2,], na.rm=TRUE)
+wmeanM <- colMeans(par_growthDeriv[par_strain==2 & par_sex==2,], na.rm=TRUE)
+gmeanF <- colMeans(par_growthDeriv[par_strain==1 & par_sex==1,], na.rm=TRUE)
+wmeanF <- colMeans(par_growthDeriv[par_strain==2 & par_sex==1,], na.rm=TRUE)
+f2meanM <- colMeans(growthDeriv[f2$pheno$sex=="M",], na.rm=TRUE)
+f2meanF <- colMeans(growthDeriv[f2$pheno$sex=="F",], na.rm=TRUE)
+
+pdf("../Figs/rate1.pdf", height=5, width=10, pointsize=16)
+par(bg=bg, fg="white", col="white", col.axis="white", col.lab="white", col.main="white")
+par(las=1, mar=c(4.1, 4.1, 1.1, 0.6), mfrow=c(1,2))
 xat <- c(5, 10, 15)
 yat <- seq(-2, 5, by=1)
 grayplot(0,0,type="n", xlim=c(1, 16), xaxs="i", ylim=yli, yaxs="i",
          xlab="Week", ylab="Growth rate (g/week)",
          xat=c(1,5,10,15), vlines=xat, vlines.col="gray70",vlines.lwd=3,
-         yat=yat, hlines=yat)
+         yat=yat, hlines=yat, main="Males")
 for(i in sample(which(f2$pheno$sex=="M"))) {
   y <- growthDeriv[i,1:16]
   x <- (1:16)[!is.na(y)]
   y <- y[!is.na(y)]
   lines(x, y, lwd=1, col=colalpha[2])
 }
-f2mean <- colMeans(growthDeriv[f2$pheno$sex=="M",], na.rm=TRUE)
-lines(1:16, f2mean, col=brocolors("crayons")["Navy Blue"], lwd=2)
-dev.off()    
+lines(1:16, f2meanM, col=col[4], lwd=2)
+grayplot(0,0,type="n", xlim=c(1, 16), xaxs="i", ylim=yli, yaxs="i",
+         xlab="Week", ylab="Growth rate (g/week)",
+         xat=c(1,5,10,15), vlines=xat, vlines.col="gray70",vlines.lwd=3,
+         yat=yat, hlines=yat, main="Females")
+for(i in sample(which(f2$pheno$sex=="F"))) {
+  y <- growthDeriv[i,1:16]
+  x <- (1:16)[!is.na(y)]
+  y <- y[!is.na(y)]
+  lines(x, y, lwd=1, col=colalpha[1])
+}
+lines(1:16, f2meanF, col=col[3], lwd=2)
+dev.off()
 
 
-pdf("../Figs/rate2.pdf", height=6.5, width=10, pointsize=20)
-par(bg=bg, fg="white", col="white", col.axis="white", col.lab="white")
-par(las=1, mar=c(4.1, 4.1, 0.6, 3.1))
+pdf("../Figs/rate2.pdf", height=5, width=10, pointsize=16)
+par(bg=bg, fg="white", col="white", col.axis="white", col.lab="white", col.main="white")
+par(las=1, mar=c(4.1, 4.1, 1.1, 0.6), mfrow=c(1,2))
 xat <- c(5, 10, 15)
 yat <- seq(-2, 5, by=1)
 grayplot(0,0,type="n", xlim=c(1, 16), xaxs="i", ylim=yli, yaxs="i",
          xlab="Week", ylab="Growth rate (g/week)",
          xat=c(1,5,10,15), vlines=xat, vlines.col="gray70",vlines.lwd=3,
-         yat=yat, hlines=yat)
+         yat=yat, hlines=yat, main="Males")
 for(i in sample(which(f2$pheno$sex=="M"))) {
   y <- growthDeriv[i,1:16]
   x <- (1:16)[!is.na(y)]
   y <- y[!is.na(y)]
   lines(x, y, lwd=1, col=colalpha[2])
 }
-f2mean <- colMeans(growthDeriv[f2$pheno$sex=="M",], na.rm=TRUE)
-lines(1:16, f2mean, col=brocolors("crayons")["Navy Blue"], lwd=2)
-gmean <- colMeans(par_growthDeriv[par_strain==1 & par_sex==2,], na.rm=TRUE)
-wmean <- colMeans(par_growthDeriv[par_strain==2 & par_sex==2,], na.rm=TRUE)
-lines(1:16, gmean, col=col[6], lwd=2)
-lines(1:16, wmean, col=col[4], lwd=2)
-text(5.5, 3.5, "Gough", col=col[6], adj=c(0, 0.5))
-text(4.5, 0.5, "WSB", col=col[4], adj=c(1, 0.5))
-dev.off()    
-
-
-pdf("../Figs/rate3.pdf", height=6.5, width=10, pointsize=20)
-par(bg=bg, fg="white", col="white", col.axis="white", col.lab="white")
-par(las=1, mar=c(4.1, 4.1, 0.6, 3.1))
-xat <- c(5, 10, 15)
-yat <- seq(-2, 5, by=1)
+lines(1:16, f2meanF, col=col[3], lwd=2)
+lines(1:16, f2meanM, col=col[4], lwd=2)
 grayplot(0,0,type="n", xlim=c(1, 16), xaxs="i", ylim=yli, yaxs="i",
          xlab="Week", ylab="Growth rate (g/week)",
          xat=c(1,5,10,15), vlines=xat, vlines.col="gray70",vlines.lwd=3,
-         yat=yat, hlines=yat)
+         yat=yat, hlines=yat, main="Females")
 for(i in sample(which(f2$pheno$sex=="F"))) {
   y <- growthDeriv[i,1:16]
   x <- (1:16)[!is.na(y)]
   y <- y[!is.na(y)]
   lines(x, y, lwd=1, col=colalpha[1])
 }
-f2mean <- colMeans(growthDeriv[f2$pheno$sex=="F",], na.rm=TRUE)
-lines(1:16, f2mean, col=brocolors("crayons")["Radical Red"], lwd=2)
-dev.off()    
+lines(1:16, f2meanM, col=col[4], lwd=2)
+lines(1:16, f2meanF, col=col[3], lwd=2)
+dev.off()
 
 
-pdf("../Figs/rate4.pdf", height=6.5, width=10, pointsize=20)
-par(bg=bg, fg="white", col="white", col.axis="white", col.lab="white")
-par(las=1, mar=c(4.1, 4.1, 0.6, 3.1))
+pdf("../Figs/rate3.pdf", height=5, width=10, pointsize=16)
+par(bg=bg, fg="white", col="white", col.axis="white", col.lab="white", col.main="white")
+par(las=1, mar=c(4.1, 4.1, 1.1, 0.6), mfrow=c(1,2))
 xat <- c(5, 10, 15)
 yat <- seq(-2, 5, by=1)
 grayplot(0,0,type="n", xlim=c(1, 16), xaxs="i", ylim=yli, yaxs="i",
          xlab="Week", ylab="Growth rate (g/week)",
          xat=c(1,5,10,15), vlines=xat, vlines.col="gray70",vlines.lwd=3,
-         yat=yat, hlines=yat)
+         yat=yat, hlines=yat, main="Males")
+for(i in sample(which(f2$pheno$sex=="M"))) {
+  y <- growthDeriv[i,1:16]
+  x <- (1:16)[!is.na(y)]
+  y <- y[!is.na(y)]
+  lines(x, y, lwd=1, col=colalpha[2])
+}
+lines(1:16, f2meanM, col=col[4], lwd=2)
+lines(1:16, gmeanM, col=col[6], lwd=2)
+lines(1:16, wmeanM, col=col[8], lwd=2)
+text(3.8, 3.0, expression(F[2]), col=col[4], adj=c(0, 0.5))
+text(5.3, 3.5, "Gough", col=col[6], adj=c(0, 0.5))
+text(5.0, 0.7, "WSB", col=col[8], adj=c(1, 0.5))
+grayplot(0,0,type="n", xlim=c(1, 16), xaxs="i", ylim=yli, yaxs="i",
+         xlab="Week", ylab="Growth rate (g/week)",
+         xat=c(1,5,10,15), vlines=xat, vlines.col="gray70",vlines.lwd=3,
+         yat=yat, hlines=yat, main="Females")
 for(i in sample(which(f2$pheno$sex=="F"))) {
   y <- growthDeriv[i,1:16]
   x <- (1:16)[!is.na(y)]
   y <- y[!is.na(y)]
   lines(x, y, lwd=1, col=colalpha[1])
 }
-f2mean <- colMeans(growthDeriv[f2$pheno$sex=="F",], na.rm=TRUE)
-lines(1:16, f2mean, col=brocolors("crayons")["Radical Red"], lwd=2)
-gmean <- colMeans(par_growthDeriv[par_strain==1 & par_sex==1,], na.rm=TRUE)
-wmean <- colMeans(par_growthDeriv[par_strain==2 & par_sex==1,], na.rm=TRUE)
-lines(1:16, gmean, col=col[5], lwd=2)
-lines(1:16, wmean, col=col[3], lwd=2)
-text(5.5, 2.5, "Gough", col=col[5], adj=c(0, 0.5))
-text(3.5, 0.5, "WSB", col=col[3], adj=c(1, 0.5))
-dev.off()    
-
-
+lines(1:16, f2meanF, col=col[3], lwd=2)
+lines(1:16, gmeanF, col=col[5], lwd=2)
+lines(1:16, wmeanF, col=col[7], lwd=2)
+text(1.6, 3.0, expression(F[2]), col=col[3], adj=c(0, 0.5))
+text(2.8, 3.5, "Gough", col=col[5], adj=c(0, 0.5))
+text(4.0, 1.2, "WSB", col=col[7], adj=c(1, 0.5))
+dev.off()
