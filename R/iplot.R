4f63977283337af6373761d50e2d4d5774d185e9
Karl Broman <kbroman@gmail.com>
2017-03-08 19:22:17 -0600

    Update interactive plots with all weeks' data

--- R/iplot.R
+++ R/iplot.R
@@ -1,9 +1,10 @@
+library(qtl)
 library(qtlcharts)
 
 file <- "iplot_cache.RData"
 if(file.exists(file)) {
   load(file)
-} else {  
+} else {
   load("~/Projects/Payseur_Gough/DerivedData/goughF2_simple_v4.RData")
   load("~/Projects/Payseur_Gough/R/scanone_2014-04-23.RData")
 
@@ -17,12 +18,20 @@ if(file.exists(file)) {
   f2sm$pheno[,1:16] <- growthSm
   f2d$pheno[,1:16] <- growthDeriv
 
+  muFemale <- estQTLeffects(f2sm[,sex==0], phe=1:16, "means")
+  muMale <- estQTLeffects(f2sm[,sex==1], phe=1:16, "means")
+  mu <- cbindQTLeffects(muFemale, muMale, labels=c("female", "male"))
+
   effFemale <- estQTLeffects(f2sm[,sex==0], phe=1:16, "effects")
   effMale <- estQTLeffects(f2sm[,sex==1], phe=1:16, "effects")
   effFemale <- lapply(effFemale, function(a) -a)
   effMale <- lapply(effMale, function(a) -a)
-  eff <- cbindQTLeffects(effFemale, effMale, labels=c("female", "male"))
+  eff <- cbindQTLeffects(effFemale, effMale, labels=c("f", "m"))
   outsm_signed <- qtlcharts:::calcSignedLOD(outsm, eff, columns=c(1,3))
+
+  dmuFemale <- estQTLeffects(f2d[,sex==0], phe=1:16, "means")
+  dmuMale <- estQTLeffects(f2d[,sex==1], phe=1:16, "means")
+  dmu <- cbindQTLeffects(dmuFemale, dmuMale, labels=c("f", "m"))
 
   deffFemale <- estQTLeffects(f2d[,sex==0], phe=1:16, "effects")
   deffMale <- estQTLeffects(f2d[,sex==1], phe=1:16, "effects")
@@ -31,39 +40,32 @@ if(file.exists(file)) {
   deff <- cbindQTLeffects(deffFemale, deffMale, labels=c("female", "male"))
   outd_signed <- qtlcharts:::calcSignedLOD(outd, deff, columns=c(1,3))
 
-  save(outsm_signed, eff, outd_signed, deff, file=file)
+  save(outsm_signed, mu, outd_signed, dmu, file=file)
 }
 
-file <- "../iplot_bodyweight.html"
+color <- brocolors("crayons")[c("Jungle Green", "Mango Tango", "Purple Mountain's Majesty",
+                                "Jungle Green", "Mango Tango", "Purple Mountain's Majesty")]
+names(color) <- NULL
+
+
+setScreenSize(height=800, width=1066)
+
+file <- "iplot_bodyweight.html"
 if(file.exists(file)) unlink(file)
-iplotMScanone(outsm_signed, lod=1:16, effects=eff,
-              title="Body weight", openfile=FALSE,
-              onefile=TRUE, file=file,
-              chartOpts=list(eff_linecolor=c("red", "pink", "blue", "lightblue"),
-                             eff_ylab="QTL effect"),
-              caption=c("In the heat map at the top, red indicates that the Gough allele increases body weight, ",
-                "and blue indicates that the Gough allele decreases body weight. Hover over the heat map to view ",
-                "individual LOD curves below and estimated QTL effects to the right. Regarding the estimated QTL effects, ",
-                "on the autosomes, red and pink are for the additive and dominance effects, respectively, in females, while ",
-                "blue and light blue are the additive and dominance effects, respectively, in males. ",
-                "On the X chromosome, red and pink are for the additive effects in females from the G&times;W and W&times;G crosses, ",
-                "respectively, while blue is for the additive effect in males. Positive values indicate that the Gough allele ",
-                "increases body weight."))
+z <- iplotMScanone(outsm_signed, lod=1:16, times=1:16, effects=mu,
+              chartOpts=list(eff_linecolor=color,
+                             eff_ylab="Body weight (g)",
+                             ylab="Week",
+                             margin=list(left=60, top=40, right=60, bottom=40, inner=0)))
+htmlwidgets::saveWidget(z, file=file)
+system(paste("mv", file , ".."))
 
-
-
-file <- "../iplot_deriv_bodyweight.html"
+file <- "iplot_deriv_bodyweight.html"
 if(file.exists(file)) unlink(file)
-iplotMScanone(outd_signed, lod=1:16, effects=deff,
-              title="Derivative of body weight", openfile=FALSE,
-              onefile=TRUE, file=file,
-              chartOpts=list(eff_linecolor=c("red", "pink", "blue", "lightblue"),
-                             eff_ylab="QTL effect"),
-              caption=c("In the heat map at the top, red indicates that the Gough allele increases the rate of growth, ",
-                "and blue indicates that the Gough allele decreases the rate of growth. Hover over the heat map to view ",
-                "individual LOD curves below and estimated QTL effects to the right. Regarding the estimated QTL effects, ",
-                "on the autosomes, red and pink are for the additive and dominance effects, respectively, in females, while ",
-                "blue and light blue are the additive and dominance effects, respectively, in males. ",
-                "On the X chromosome, red and pink are for the additive effects in females from the G&times;W and W&times;G crosses, ",
-                "respectively, while blue is for the additive effect in males. Positive values indicate that the Gough allele ",
-                "increases the rate of growth."))
+z <- iplotMScanone(outd_signed, lod=1:16, times=1:16, effects=dmu,
+                   chartOpts=list(eff_linecolor=color,
+                                  eff_ylab="Growth rate (g/week)",
+                                  ylab="Week",
+                             margin=list(left=60, top=40, right=60, bottom=40, inner=0)))
+htmlwidgets::saveWidget(z, file=file)
+system(paste("mv", file , ".."))
